5fd781d229766bc38bf0bba36f514cb08374e0e0
Juan <Erikjuan32@gmail.com>
2025-08-11 19:25:32 -0300

    update

---
+++ exercicios/Exercícios/Ex005/script.js
@@ -0,0 +1,15 @@
+function Adicionar() {
+    let num = document.getElementById(`txtn`)
+    let add = document.getElementById(`selvel`)
+    let valor = (num.value)
+    valor = []
+
+    if (num.value.length == 0 || num.value.length > 100 && num.value.length < 1) {
+        window.alert("O valor inserido é ínvalido!")
+    } else {
+            valor.push
+            let item = document.createElement("option")
+            item.text = `O Valor ${valor} foi adicionado`
+            add.appendChild(item)
+    }
+}
