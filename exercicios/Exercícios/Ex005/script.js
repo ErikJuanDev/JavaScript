981aa3b5cb9ca965350df4ad4bb2e25f77ddadd9
Juan <Erikjuan32@gmail.com>
2025-08-12 17:36:19 -0300

    Projeto Analisador Numérico

--- exercicios/Exercícios/Ex005/script.js
+++ exercicios/Exercícios/Ex005/script.js
@@ -1,15 +1,66 @@
-function Adicionar() {
-    let num = document.getElementById(`txtn`)
-    let add = document.getElementById(`selvel`)
-    let valor = (num.value)
-    valor = []
+let num = document.getElementById(`txtn`)
+let lista = document.getElementById(`selvel`)
+let r = document.getElementById(`res`)
+valor = []
 
-    if (num.value.length == 0 || num.value > 100 || num.value < 1) {
-        window.alert("O valor inserido é ínvalido!")
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) > -1){
+        return true
     } else {
-            valor.push
-            let item = document.createElement("option")
-            item.text = `O Valor ${valor} foi adicionado`
-            add.appendChild(item)
+        return false
     }
 }
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
+}
+    function Adicionar(){
+        if (isNumero(num.value) && !inLista(num.value, valor)){
+            valor.push(Number(num.value))
+            let item = document.createElement("option")
+            item.text = `O Valor ${num.value} foi adicionado`
+            lista.appendChild(item)
+            
+        } else {
+            window.alert("Valor não encontrado ou já está na lista!")
+        }
+        num.value = ""
+        num.focus()
+    }
+
+    function Final(){
+    if (valor.length != 0){
+        const comprimento = (valor.length)
+        let maior = valor[0]
+        let menor = valor[0]
+        let soma = 0 
+        let media = 0
+        for(let val in valor){
+            soma += valor[val]
+            if(maior < valor[val]){
+                maior = valor[val]
+            } else {
+                menor = valor[val]
+            }
+            media = soma / valor.length
+        }
+
+        r.innerHTML = ""
+        //comprimento
+        r.innerHTML += `<p>O total de valores adicionados é ${comprimento}</p>`
+        //maior valor
+        r.innerHTML += `<p>O maior valor inserido é ${maior}.</p>`
+        //menor valor
+        r.innerHTML += `<p>O menor valor inserido  ${menor}.</p>`
+        //Soma
+        r.innerHTML += `<p>A soma dos valores inseridos é ${soma}.</p>`
+        //Média
+        r.innerHTML += `<p>A média dos valores inseridos é ${media}.</p>`
+    } else {
+        window.alert(`Adicione qualquer valor na lista para prosseguir!`)
+    }
+    }
